903665cd8179d7132a7ceb96ff47686b12b3fdfa
HariharanJ_lumel <hariharanj@lumel.com>
2025-10-06 21:17:54 +0530

    feat(header/hero) header and hero components added

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+
+import HeroSecttion from '../components/Hero'
+import { Button } from '../components/ui/button'
+
+const page = () => {
+  return (
+    <>
+      <div className='grid-background'></div>
+      <HeroSecttion />
+    </>
+  )
+}
+
+export default page
---
+++ components/Hero.jsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+
+const HeroSecttion = () => {
+    return (
+        <section className='w-full pt-36 md:pt-48 pb-10'>
+            <div className='space-y-6 text-center'>
+                <div className='space-y-6 mx-auto'>
+                    <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title'>
+                        Your AI Career Coach for
+                        <br />
+                        Professional Success
+                    </h1>
+                    <p>Advance your career with personalized guidance, interview prep, and AI-powered tools for job Success</p>
+                </div>
+
+                <div>
+                    <Link href={"/dashboard"}>
+                        <Button size={"lg"} className={"px-8"}>Get Started</Button>
+                    </Link>
+                </div>
+
+                <div>
+                    <div>
+                        <Image
+                            src={"/banner.jpeg"}
+                            width={1280}
+                            height={720}
+                            alt='banner'
+                            className='rounded-lg shadow-2xl border mx-auto'
+                            priority
+                        ></Image>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HeroSecttion 
