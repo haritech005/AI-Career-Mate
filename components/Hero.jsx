f36ef66d629a507836a8ff8508cd9b953e44ee8d
Hariharan J <135329695+haritech005@users.noreply.github.com>
2025-10-06 22:16:08 +0530

    Hero section UI updated

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,30 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 
 const HeroSecttion = () => {
+    const imageRef = useRef(null);
+    
+    useEffect(() => {
+        const imageElement = imageRef.current;
+
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+
+            if (scrollPosition > scrollThreshold) {
+                imageElement.classList.add("scrolled");
+            } else {
+                imageElement.classList.remove("scrolled");
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <section className='w-full pt-36 md:pt-48 pb-10'>
             <div className='space-y-6 text-center'>
@@ -13,7 +34,7 @@ const HeroSecttion = () => {
                         <br />
                         Professional Success
                     </h1>
-                    <p>Advance your career with personalized guidance, interview prep, and AI-powered tools for job Success</p>
+                    <p className='mx-auto max-w-[600px] text-muted-foreground md:text-xl'>Advance your career with personalized guidance, interview prep, and AI-powered tools for job Success</p>
                 </div>
 
                 <div>
@@ -22,8 +43,8 @@ const HeroSecttion = () => {
                     </Link>
                 </div>
 
-                <div>
-                    <div>
+                <div className='hero-image-wrapper mt-5 md:mt-0'>
+                    <div ref={imageRef} className='hero-image'>
                         <Image
                             src={"/banner.jpeg"}
                             width={1280}
